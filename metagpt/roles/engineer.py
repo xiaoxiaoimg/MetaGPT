c89dc92a85c2cecb18a99df376d6d0f8f6825031
zhangfuqiang <zhangfuqiang@fuzhi.ai>
2024-07-09 12:36:03 +0000

    Fix: Added None check for code_plan_and_change_doc to prevent AttributeError

--- metagpt/roles/engineer.py
+++ metagpt/roles/engineer.py
@@ -117,9 +117,8 @@ class Engineer(Role):
                 coding_context = await action.run()
 
             dependencies = {coding_context.design_doc.root_relative_path, coding_context.task_doc.root_relative_path}
-            if self.config.inc:
+            if coding_context.code_plan_and_change_doc is not None:
                 dependencies.add(coding_context.code_plan_and_change_doc.root_relative_path)
-            await self.project_repo.srcs.save(
                 filename=coding_context.filename,
                 dependencies=list(dependencies),
                 content=coding_context.code_doc.content,
