984ba0f6115bb4621b119acbc246919526f24276
zhangfuqiang <zhangfuqiang@fuzhi.ai>
2024-07-09 12:56:14 +0000

    Fix: Ensure correct data is passed to CollectLinks in Researcher role

--- metagpt/roles/researcher.py
+++ metagpt/roles/researcher.py
@@ -52,7 +52,6 @@ class Researcher(Role):
             self.set_todo(None)
             return False
 
-    async def _act(self) -> Message:
         logger.info(f"{self._setting}: to do {self.rc.todo}({self.rc.todo.name})")
         todo = self.rc.todo
         msg = self.rc.memory.get(k=1)[0]
@@ -61,13 +60,34 @@ class Researcher(Role):
             topic = instruct_content.topic
         else:
             topic = msg.content
-
+        logger.info(f"Passing topic to CollectLinks: {topic}")
         research_system_text = self.research_system_text(topic, todo)
         if isinstance(todo, CollectLinks):
             links = await todo.run(topic, 4, 4)
             ret = Message(
                 content="", instruct_content=Report(topic=topic, links=links), role=self.profile, cause_by=todo
             )
+        elif isinstance(todo, WebBrowseAndSummarize):
+            links = instruct_content.links
+            todos = (todo.run(*url, query=query, system_text=research_system_text) for (query, url) in links.items())
+            summaries = await asyncio.gather(*todos)
+            summaries = list((url, summary) for i in summaries for (url, summary) in i.items() if summary)
+            ret = Message(
+                content="", instruct_content=Report(topic=topic, summaries=summaries), role=self.profile, cause_by=todo
+            )
+        else:
+            summaries = instruct_content.summaries
+            summary_text = "\n---\n".join(f"url: {url}\nsummary: {summary}" for (url, summary) in summaries)
+            content = await self.rc.todo.run(topic, summary_text, system_text=research_system_text)
+            ret = Message(
+                content="",
+                instruct_content=Report(topic=topic, content=content),
+                role=self.profile,
+                cause_by=self.rc.todo,
+            )
+        self.rc.memory.add(ret)
+        return ret
+        return ret
         elif isinstance(todo, WebBrowseAndSummarize):
             links = instruct_content.links
             todos = (todo.run(*url, query=query, system_text=research_system_text) for (query, url) in links.items())
