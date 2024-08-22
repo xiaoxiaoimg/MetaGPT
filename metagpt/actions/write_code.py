238ce7c2ff999b41a0ac125eb5c7a3de5efd5b20
zhangfuqiang <zhangfuqiang@fuzhi.ai>
2024-08-22 12:31:02 +0800

    Fix Issue #1210: Ensure WriteCode.run is called correctly during incremental development

--- metagpt/actions/write_code.py
+++ metagpt/actions/write_code.py
@@ -90,62 +90,62 @@ class WriteCode(Action):
         code = CodeParser.parse_code(block="", text=code_rsp)
         return code
 
-    async def run(self, *args, **kwargs) -> CodingContext:
-        bug_feedback = await self.repo.docs.get(filename=BUGFIX_FILENAME)
-        coding_context = CodingContext.loads(self.i_context.content)
-        test_doc = await self.repo.test_outputs.get(filename="test_" + coding_context.filename + ".json")
-        requirement_doc = await self.repo.docs.get(filename=REQUIREMENT_FILENAME)
-        summary_doc = None
-        if coding_context.design_doc and coding_context.design_doc.filename:
-            summary_doc = await self.repo.docs.code_summary.get(filename=coding_context.design_doc.filename)
-        logs = ""
-        if test_doc:
-            test_detail = RunCodeResult.loads(test_doc.content)
-            logs = test_detail.stderr
+async def run(self, *args, **kwargs) -> CodingContext:
+    bug_feedback = await self.repo.docs.get(filename=BUGFIX_FILENAME)
+    coding_context = CodingContext.loads(self.i_context.content)
+    test_doc = await self.repo.test_outputs.get(filename="test_" + coding_context.filename + ".json")
+    requirement_doc = await self.repo.docs.get(filename=REQUIREMENT_FILENAME)
+    summary_doc = None
+    if coding_context.design_doc and coding_context.design_doc.filename:
+        summary_doc = await self.repo.docs.code_summary.get(filename=coding_context.design_doc.filename)
+    logs = ""
+    if test_doc:
+        test_detail = RunCodeResult.loads(test_doc.content)
+        logs = test_detail.stderr
 
-        if bug_feedback:
-            code_context = coding_context.code_doc.content
-        elif self.config.inc:
-            code_context = await self.get_codes(
-                coding_context.task_doc, exclude=self.i_context.filename, project_repo=self.repo, use_inc=True
-            )
-        else:
-            code_context = await self.get_codes(
-                coding_context.task_doc,
-                exclude=self.i_context.filename,
-                project_repo=self.repo.with_src_path(self.context.src_workspace),
-            )
+    if bug_feedback:
+        code_context = coding_context.code_doc.content
+    elif self.config.inc:
+        code_context = await self.get_codes(
+            coding_context.task_doc, exclude=self.i_context.filename, project_repo=self.repo, use_inc=True
+        )
+    else:
+        code_context = await self.get_codes(
+            coding_context.task_doc,
+            exclude=self.i_context.filename,
+            project_repo=self.repo.with_src_path(self.context.src_workspace),
+        )
 
-        if self.config.inc:
-            prompt = REFINED_TEMPLATE.format(
-                user_requirement=requirement_doc.content if requirement_doc else "",
-                code_plan_and_change=str(coding_context.code_plan_and_change_doc),
-                design=coding_context.design_doc.content if coding_context.design_doc else "",
-                task=coding_context.task_doc.content if coding_context.task_doc else "",
-                code=code_context,
-                logs=logs,
-                feedback=bug_feedback.content if bug_feedback else "",
-                filename=self.i_context.filename,
-                summary_log=summary_doc.content if summary_doc else "",
-            )
-        else:
-            prompt = PROMPT_TEMPLATE.format(
-                design=coding_context.design_doc.content if coding_context.design_doc else "",
-                task=coding_context.task_doc.content if coding_context.task_doc else "",
-                code=code_context,
-                logs=logs,
-                feedback=bug_feedback.content if bug_feedback else "",
-                filename=self.i_context.filename,
-                summary_log=summary_doc.content if summary_doc else "",
-            )
-        logger.info(f"Writing {coding_context.filename}..")
-        code = await self.write_code(prompt)
-        if not coding_context.code_doc:
-            # avoid root_path pydantic ValidationError if use WriteCode alone
-            root_path = self.context.src_workspace if self.context.src_workspace else ""
-            coding_context.code_doc = Document(filename=coding_context.filename, root_path=str(root_path))
-        coding_context.code_doc.content = code
-        return coding_context
+    if self.config.inc:
+        prompt = REFINED_TEMPLATE.format(
+            user_requirement=requirement_doc.content if requirement_doc else "",
+            code_plan_and_change=str(coding_context.code_plan_and_change_doc),
+            design=coding_context.design_doc.content if coding_context.design_doc else "",
+            task=coding_context.task_doc.content if coding_context.task_doc else "",
+            code=code_context,
+            logs=logs,
+            feedback=bug_feedback.content if bug_feedback else "",
+            filename=self.i_context.filename,
+            summary_log=summary_doc.content if summary_doc else "",
+        )
+    else:
+        prompt = PROMPT_TEMPLATE.format(
+            design=coding_context.design_doc.content if coding_context.design_doc else "",
+            task=coding_context.task_doc.content if coding_context.task_doc else "",
+            code=code_context,
+            logs=logs,
+            feedback=bug_feedback.content if bug_feedback else "",
+            filename=self.i_context.filename,
+            summary_log=summary_doc.content if summary_doc else "",
+        )
+    logger.info(f"Writing {coding_context.filename}..")
+    code = await self.write_code(prompt)
+    if not coding_context.code_doc:
+        # avoid root_path pydantic ValidationError if use WriteCode alone
+        root_path = self.context.src_workspace if self.context.src_workspace else ""
+        coding_context.code_doc = Document(filename=coding_context.filename, root_path=str(root_path))
+    coding_context.code_doc.content = code
+    return coding_context
 
     @staticmethod
     async def get_codes(task_doc: Document, exclude: str, project_repo: ProjectRepo, use_inc: bool = False) -> str:
